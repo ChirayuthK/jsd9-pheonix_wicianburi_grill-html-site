6821994e9e6968b7b673c56601392e1e1438ff9d
ChirayuthK <chirayuth40489@gmail.com>
2025-04-03 10:45:30 +0700

    Fixed verceljson viteconfig js Navbar

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,4 +1,3 @@
-// Navbar component
 export class NavBar extends HTMLElement {
     constructor() {
         super();
@@ -10,7 +9,7 @@ export class NavBar extends HTMLElement {
                 <div class="text-gray-600 sm:hidden">
                     <span class="material-symbols-outlined">menu</span>
                 </div>
-                <a href="/index.html" class="flex gap-2 items-center">
+                <a href="/" class="flex gap-2 items-center">
                     <img
                         src="/assets/logo-all_rice.svg"
                         alt="All rice logo"
@@ -19,20 +18,20 @@ export class NavBar extends HTMLElement {
                     <p class="text-sm font-black">All Rice Co.</p>
                 </a>
                 <ul class="hidden sm:flex sm:gap-4 sm:font-bold">
-                    <li><a href="/index.html">Home</a></li>
-                    <li><a href="/src/pages/aboutUs.html">About Us</a></li>
-                    <li><a href="/src/pages/product-list.html">Product</a></li>
-                    <li><a href="/src/pages/contactUs.html">Contact</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/aboutUs">About Us</a></li>
+                    <li><a href="/product-list">Product</a></li>
+                    <li><a href="/contactUs">Contact</a></li>
                 </ul>
                 <div class="flex gap-4">
                     <div class="text-gray-600">
-                        <a href="/src/pages/cart.html" target="_self">
+                        <a href="/cart" target="_self">
                             <span class="material-symbols-outlined block">shopping_cart</span>
                         </a>
                     </div>
 
                     <div class="text-gray-600 hidden sm:block">
-                        <a href="/src/pages/login.html" target="_self">
+                        <a href="/login" target="_self">
                             <span class="material-symbols-outlined block">account_circle</span>
                         </a>
                     </div>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+import { resolve } from 'path'
+import fs from 'fs'
 
+const htmlPages = fs.readdirSync('./src/pages')
+  .filter(file => file.endsWith('.html'))
+  .reduce((entries, file) => {
+    const name = file.replace('.html', '');
+    entries[name] = resolve(__dirname, `src/pages/${file}`);
+    return entries;
+  }, {
+    main: resolve(__dirname, 'index.html'),
+  });
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: htmlPages,
+    },
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+});
